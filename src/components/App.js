71782c58390b31b06f1b01dbfbcd022a269625f6
Paduchuri Krishna Vamsi <62980871+krishna905@users.noreply.github.com>
2022-08-29 19:07:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
+import Display from './display'
 
 function App() {
   return (
     <div id="main">
       // Do not alter the main div
+    <Display/>
     </div>
   );
 }
