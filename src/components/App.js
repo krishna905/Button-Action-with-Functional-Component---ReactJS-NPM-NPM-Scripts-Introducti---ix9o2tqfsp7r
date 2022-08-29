824f28c5dcc79bf7f18f3571a4ff2e77a23e67a1
Paduchuri Krishna Vamsi <62980871+krishna905@users.noreply.github.com>
2022-08-29 19:19:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
-import Display from './display'
+
 
 function App() {
+   const [id, setId] = useState(false)
   return (
     <div id="main">
-      // Do not alter the main div
-    <Display/>
+      <button id="click" onClick={() => setId(true)}>display</button>
+            <>
+                {
+                    id ? <div>
+                        Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+                    </div> : null
+                }
+            </>
     </div>
   );
 }
